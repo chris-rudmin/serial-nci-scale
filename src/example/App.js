a44de514c937a6e9fac8b3c4126c4fcb94f12250
Christopher Rudmin <chris.rudmin@gmail.com>
2021-01-13 11:17:46 -0500

    Rename lib

--- src/example/App.js
+++ src/example/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import SerialWebUSBScale from '../serial-webusb-scale';
+import SerialNCIScale from '../serial-nci-scale';
 import { Button } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
